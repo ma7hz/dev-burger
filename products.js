f59f9b9a7ce5d0e86ccc20467c018bf0a84c429d
ma7hz <henn.ma77@gmail.com>
2024-04-29 22:34:46 -0300

    mark one

---
+++ products.js
@@ -0,0 +1,10 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, src: './assets/x salada.jpeg' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './assets/x bacon.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/x eggbacon.png' },
+    { name: 'Potência', price: 50, vegan: false, src: './assets/x potencia.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './assets/potencia vegan.png' },
+    { name: 'X-Vegan', price: 45, vegan: true, src: './assets/vegan.png' },
+    ]
+    
+const newPrices = []
